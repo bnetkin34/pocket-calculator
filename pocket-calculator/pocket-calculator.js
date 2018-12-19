effd8d252930da241867513f8c50c7462e5ec1c6
Brett Netkin <sweatynettie@Bretts-MacBook-Pro.local>
2018-12-19 13:22:50 -0500

    initializing p2

---
+++ pocket-calculator/pocket-calculator.js
@@ -0,0 +1,49 @@
+function button0(){
+let p = document.getElementbyId("output");
+p.innerHTML = 0;
+}
+
+function button1(){
+let p = document.getElementbyId("output");
+p.innerHTML = 1;
+}
+
+function button2(){
+  let p = document.getElementbyId("output");
+  p.innerHTML = 2;
+}
+
+function button3(){
+  let p = document.getElementbyId("output");
+  p.innerHTML = 3;
+}
+
+function button4(){
+  let p = document.getElementbyId("output");
+  p.innerHTML = 4;
+}
+
+function button5(){
+  let p = document.getElementbyId("output");
+  p.innerHTML = 5;
+}
+
+function button6(){
+  let p = document.getElementbyId("output");
+  p.innerHTML = 6;
+}
+
+function button7(){
+  let p = document.getElementbyId("output");
+  p.innerHTML = 7;
+}
+
+function button8(){
+  let p = document.getElementbyId("output");
+  p.innerHTML = 8;
+}
+
+function button9(){
+  let p = document.getElementbyId("output");
+  p.innerHTML = 9;
+}
