a9e6d8cccac154d05730da508c051fe6802efd23
Brett Netkin <sweatynettie@Bretts-MacBook-Pro.local>
2019-01-08 15:19:44 -0500

    displaying too many digits

--- pocket-calculator/pocket-calculator.js
+++ pocket-calculator/pocket-calculator.js
@@ -1,4 +1,56 @@
-// // var evalStr = ""
+var evaluateStr = '';
+var lastNum = true; var lastSym = false; var lastEql = false;
+
+function insert(num){
+  if (lastEql){
+    clean(); lastEql = false;
+  }
+  if (lastNum){
+    document.calculator.display.value = (document.calculator.display.value + num);
+    lastSym = true;
+  } else {
+    document.calculator.display.value = (num);
+    lastSym = true; lastNum = true;
+  }
+    evaluateStr = evaluateStr + num;
+
+    if(Number(document.calculator.display.value.length) > 11){
+    document.calculator.display.value = "Too many digits";
+    }
+}
+function parseSym(sym){
+  if (lastSym){
+    document.calculator.display.value = (sym);
+    lastSym = false; lastNum = false; lastEql = false; evaluateStr = evaluateStr + sym;
+  }
+}
+function equal(){
+  document.calculator.display.value = eval(evaluateStr).toLocaleString("en");
+  evaluateStr = eval(evaluateStr); lastNum = false; lastEql = true;
+  if(Number(evaluateStr) > 1000000000) {
+         let coefficient = 0;
+         let degree;
+         for(let n = 9; coefficient < 1 || coefficient >= 10; n++) {
+           coefficient = evaluateStr / 10 ** n;
+           degree = n;
+         }
+         document.calculator.display.value = String(coefficient) + "e" + String(degree);
+  }
+}
+function clean(){
+  document.calculator.display.value = ' ';
+  evaluateStr = ''; lastNum = true; lastSym = false;
+}
+function percentage(){
+  document.calculator.display.value = document.calculator.display.value/100;
+}
+
+function negation(){
+document.calculator.display.value = document.calculator.display.value * -1;
+}
+
+
+// // var evaluateStr = ""
 // // var prevStr = ""
 // // var ops = []
 //
@@ -7,7 +59,7 @@
 //   document.calculator.display.value = document.calculator.display.value + num;
 //   // document.calculator.display.value = " "
 //   // prevStr = num
-//   // evalStr = evalStr + num;
+//   // evaluateStr = evaluateStr + num;
 //
 //   if(document.calculator.display.value.length > 12){
 //       document.calculator.display.value = "Too many digits";
@@ -47,42 +99,6 @@
 // document.calculator.display.value = document.calculator.display.value * -1;
 // }
 
-var evalStr = ''; // This defines the variable that is calculated by the eval function
-var lastNum = true; var lastSym = false; var lastEql = false;
-function insert(num){
-  if (lastEql){
-    clean(); lastEql = false;
-  }
-  if (lastNum){
-    document.calculator.display.value = (document.calculator.display.value + num);
-    lastSym = true;
-  } else {
-    document.calculator.display.value = (num);
-    lastSym = true; lastNum = true;
-  }
-    evalStr = evalStr + num;
-}
-function parseSym(sym){
-  if (lastSym){
-    document.calculator.display.value = (sym);
-    lastSym = false; lastNum = false; lastEql = false; evalStr = evalStr + sym;
-  }
-}
-function equal(){
-  document.calculator.display.value = eval(evalStr);
-  evalStr = eval(evalStr).toLocaleString("en"); lastNum = false; lastEql = true;
-}
-function clean(){
-  document.calculator.display.value = ' ';
-  evalStr = ''; lastNum = true; lastSym = false;
-}
-function percentage(){
-  document.calculator.display.value = document.calculator.display.value/100;
-}
-
-function negation(){
-document.calculator.display.value = document.calculator.display.value * -1;
-}
 
 // function button0(){
 // let p = document.getElementbyId("output");
